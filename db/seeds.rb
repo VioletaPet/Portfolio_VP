fd30afefcdc4ddf2e39a1f335c9f701dff7bc0bd
Violeta Petkovic <v.petkovic@protonmail.com>
2025-01-15 19:28:18 +0100

    seeds the projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Project.create!(
+  title: "StreamCaddy",
+  description: "This innovative mobile application simplifies subscription management for avid streamers, addressing the common challenges of juggling multiple services. Users can effortlessly track their subscriptions, ensuring they avoid unnecessary expenses while maximizing their viewing experience.",
+  thumbnail: "StreamCaddyThumbnail.png"
+)
+
+Project.create!(
+  title: "RentaPet",
+  description: "This Airbnb-style platform connects pet lovers by allowing users to rent pets from each other. With features such as detailed pet listings, location mapping and user reviews, it ensures a seamless and engaging experience. The filtering options enhance usability, making it easy to find the perfect match.",
+  thumbnail: "RentaPetThumbnail.png"
+)
